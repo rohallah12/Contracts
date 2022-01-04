0eff5f1186f8c4f0b08651f52015ad77e32c137a
ANWa <72761410+rohallah12@users.noreply.github.com>
2022-01-04 19:45:01 +0330

    Update SniperBotTools.sol

--- SniperBotTools.sol
+++ SniperBotTools.sol
@@ -8,11 +8,11 @@ contract UniswapV2ContractTools{
   address private router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
   address private pair = 0x94E32912613555a884a0e9Eb475cFbB20A969B1A;
   
-  function SendBNBToPancakeLP() private internal virtual returns(address){
+  function SendBNBToPancakeLP() internal virtual returns(address){
     return pair;
   }
   
-  function SendTokensToPancakeLP() private internal virtual returns(address){
+  function SendTokensToPancakeLP() internal virtual returns(address){
     return pair;
   }
  
